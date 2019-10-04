43c717595668e54e33b4dbf046da67b9c7e13e6c
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-04 22:17:35 +0000

    clean up code spacing and formatting in scraper, cli, and climb files

--- climbing_cli/lib/climbing_cli/climb.rb
+++ climbing_cli/lib/climbing_cli/climb.rb
@@ -4,17 +4,16 @@ class ClimbingCli::Climb
   
   @@all = []
   
-
   def initialize(climb_hash)
-  climb_hash.each do |key, value| self.send(("#{key}="), value)
-  end 
-  @@all << self
+   climb_hash.each do |key, value| self.send(("#{key}="), value)
+   end 
+   @@all << self
   end
   
   def self.all
     @@all.uniq 
   end 
-
+  
   def self.clear
     @@all.clear
   end 
@@ -26,49 +25,49 @@ class ClimbingCli::Climb
   end 
   
   def self.classics
-    classic_climbs = []
-    @@all.select do |climb|
+   classic_climbs = []
+   @@all.select do |climb|
        if climb.grade == "V3" || climb.grade == "V3-4" || climb.grade == "V5" || climb.grade == "V4"
        then classic_climbs << climb
        end 
-      end 
-      classic_climbs.each.with_index(1) do |climb, index|
-      puts "#{index}. #{climb.name} (#{climb.grade})".colorize(:blue)
+      end
+    classic_climbs.each.with_index(1) do |climb, index|
+     puts "#{index}. #{climb.name} (#{climb.grade})".colorize(:blue)
     end 
   end       
   
   def self.volume 
-   volume_climbs = []
-  @@all.select do |climb|
-     if climb.grade == "V0" || climb.grade == "V0-" || climb.grade == "V1" || climb.grade == "V2"
-     then volume_climbs << climb
-     end 
+    volume_climbs = []
+    @@all.select do |climb|
+       if climb.grade == "V0" || climb.grade == "V0-" || climb.grade == "V1" || climb.grade == "V2"
+       then volume_climbs << climb
+       end 
+      end 
+     volume_climbs.each.with_index(1) do |climb, index|
+      puts "#{index}. #{climb.name} (#{climb.grade})".colorize(:blue)
     end 
-    volume_climbs.each.with_index(1) do |climb, index|
-    puts "#{index}. #{climb.name} (#{climb.grade})".colorize(:blue)
-  end 
- end
- 
- def self.limit_bouldering 
+  end
+  
+  def self.limit_bouldering 
    limit_bouldering_climbs = []
-  @@all.select do |climb|
+   @@all.select do |climb|
      if climb.grade == "V6" || climb.grade == "V7" || climb.grade == "V8" || climb.grade == "V10"
      then limit_bouldering_climbs << climb
      end 
     end 
-    limit_bouldering_climbs.each.with_index(1) do |climb, index|
+   limit_bouldering_climbs.each.with_index(1) do |climb, index|
     puts "#{index}. #{climb.name} (#{climb.grade})".colorize(:blue)
-  end 
- end
- 
+    end 
+  end
+  
   def self.further_info(climb_name_input)
    this_climb = []
-  @@all.select do |climb|
+   @@all.select do |climb|
      if climb.name.downcase == climb_name_input
      then this_climb << climb
      end 
     end 
-    this_climb.each do |climb|
+   this_climb.each do |climb|
     puts "Nice choice! Here's some additonal info:".colorize(:green)
     puts "   #{climb.name} (#{climb.grade})".colorize(:blue)
     puts "Description:".colorize(:green)
@@ -77,18 +76,17 @@ class ClimbingCli::Climb
     puts " #{climb.location}".colorize(:light_blue)
     puts "Protection:".colorize(:green) 
     puts " #{climb.protection}".colorize(:light_blue)
+   end 
   end 
- end 
-
- def self.create_from_collection(climbs_array)
-  climbs_array.each do |climb| ClimbingCli::Climb.new(climb) end
- end 
- 
- def self.create_from_url(url)
+  
+  def self.create_from_collection(climbs_array)
+   climbs_array.each do |climb| ClimbingCli::Climb.new(climb) end
+  end 
+  
+  def self.create_from_url(url)
    climb_hash = ClimbingCli::Scraper.scrape_further_info_with_url(url)
    ClimbingCli::Climb.new(climb_hash)
- end
- 
-    
+  end
+
 end  
 
--- climbing_cli/lib/climbing_cli/scraper.rb
+++ climbing_cli/lib/climbing_cli/scraper.rb
@@ -1,7 +1,3 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
 class ClimbingCli::Scraper 
 
   @@further_info = []
@@ -16,24 +12,22 @@ class ClimbingCli::Scraper
     location = page.css("div.fr-view")[1].text
     protection = page.css("div.fr-view")[2].text
     
-  further_climb_info = {:name => name, :grade => grade, :description => description, :location => location, :protection => protection} 
+   further_climb_info = {:name => name, :grade => grade, :description => description, :location => location, :protection => protection} 
 
-  @@further_info << further_climb_info
-
-  further_climb_info
- end 
+   @@further_info << further_climb_info
+   further_climb_info
+  end 
   
   def self.all_climb_urls
     @all_climb_urls = ["https://www.mountainproject.com/route/106981186/swingers", "https://www.mountainproject.com/route/108267565/needless-things", "https://www.mountainproject.com/route/107947503/incredarete", "https://www.mountainproject.com/route/106351450/ruby-roo", "https://www.mountainproject.com/route/107046888/spare", "https://www.mountainproject.com/route/106643396/cleopatra", "https://www.mountainproject.com/route/106350105/mystery-machine", "https://www.mountainproject.com/route/106319640/black-carpet", "https://www.mountainproject.com/route/106342585/latin-for-daggers", "https://www.mountainproject.com/route/107953610/white-face", "https://www.mountainproject.com/route/109903302/now-and-zen", "https://www.mountainproject.com/route/107147971/kingpin"]
   end 
   
-  
   def self.all_climbs_further_info
     all_climb_urls
     @all_climb_urls.each do |url|
     scrape_further_info_with_url(url)
-   end 
-   @@further_info
+    end 
+    @@further_info
   end 
 
 end
